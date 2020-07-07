15d40b43687aaecce151e2ce3bf1aa8e2cc7880e
Cormac McCarty <fenianemt@gmail.com>
2020-07-07 17:47:21 -0400

    #3 City search updates

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+
+import { WeatherService } from '../weather/weather.service';
 
 @Component({
   selector: 'app-city-search',
@@ -7,9 +10,21 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./city-search.component.css'],
 })
 export class CitySearchComponent implements OnInit {
-  search = new FormControl();
+  search = new FormControl('', [Validators.minLength(2)]);
 
-  constructor() {}
+  constructor(private weatherService: WeatherService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue: string) => {
+      if (!this.search.invalid) {
+        const userInput = searchValue.split(',').map((s) => s.trim());
+        this.weatherService
+          .getCurrentWeather(
+            userInput[0],
+            userInput.length > 1 ? userInput[1] : undefined,
+          )
+          .subscribe((data) => console.log(data));
+      }
+    });
+  }
 }
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -7,7 +7,11 @@ import { environment } from '../../environments/environment';
 import { ICurrentWeather } from '../interfaces';
 
 export interface IWeatherService {
-  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather>;
+  getCurrentWeather(
+    search: string | number,
+    country?: string,
+  ): Observable<ICurrentWeather>;
+  getCurrentWeatherByCoords(coords: Coordinates): Observable<ICurrentWeather>;
 }
 
 interface ICurrentWeatherData {
@@ -33,10 +37,29 @@ interface ICurrentWeatherData {
 export class WeatherService implements IWeatherService {
   constructor(private httpClient: HttpClient) {}
 
-  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+  getCurrentWeather(
+    search: string | number,
+    country?: string,
+  ): Observable<ICurrentWeather> {
+    let uriParams = new HttpParams();
+    if (typeof search === 'string') {
+      uriParams = uriParams.set('q', country ? `${search},${country}` : search);
+    } else {
+      uriParams = uriParams.set('zip', 'search');
+    }
+
+    return this.getCurrentWeatherHelper(uriParams);
+  }
+
+  getCurrentWeatherByCoords(coords: Coordinates): Observable<ICurrentWeather> {
     const uriParams = new HttpParams()
-      .set('q', `${city},${country}`)
-      .set('appid', environment.appId);
+      .set('lat', coords.latitude.toString())
+      .set('lon', coords.longitude.toString());
+    return this.getCurrentWeatherHelper(uriParams);
+  }
+
+  private getCurrentWeatherHelper(uriParams: HttpParams): Observable<ICurrentWeather> {
+    uriParams = uriParams.set('appid', environment.appId);
     return this.httpClient
       .get<ICurrentWeatherData>(
         `${environment.baseUrl}api.openweathermap.org/data/2.5/weather`,
