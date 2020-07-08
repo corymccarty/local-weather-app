923a6beadcd54f5bff90bc915daf8aaf9e0565c5
Cormac McCarty <fenianemt@gmail.com>
2020-07-08 15:02:55 -0400

    #11 Fix the unit test

--- src/app/postal-code/postal-code.service.spec.ts
+++ src/app/postal-code/postal-code.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { PostalCodeService } from './postal-code.service';
@@ -6,7 +7,9 @@ describe('PostalCodeService', () => {
   let service: PostalCodeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(PostalCodeService);
   });
 
